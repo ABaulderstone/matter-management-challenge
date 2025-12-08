5bd832e2db18ad3929ea28d6973c8389a3771365
ABaulderstone <alex.baulderstone@gmail.com>
2025-12-08 14:00:51 +1030

    resolve debounce bug

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -3,19 +3,19 @@ import { useRef } from 'react';
 export interface SearchBarProps {
   onSearch: (value: string) => unknown;
   className?: string;
-  placeholder?: 'Search matters...';
-  debounceMs?: 500;
+  placeholder?: string;
+  debounceMs?: number;
 }
 
 export default function SearchBar({
   onSearch,
   className = '',
-  placeholder,
-  debounceMs,
+  placeholder = 'Search matters...',
+  debounceMs = 500,
 }: SearchBarProps) {
   // search state is in parent no need for duplication of state or useEffect
   const timeoutRef = useRef<number | null>(null);
-
+  const inputRef = useRef<HTMLInputElement | null>(null);
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
     if (timeoutRef.current) {
@@ -30,11 +30,15 @@ export default function SearchBar({
     if (timeoutRef.current) {
       clearTimeout(timeoutRef.current);
     }
+    if (inputRef.current) {
+      inputRef.current.value = '';
+    }
     onSearch('');
   };
   return (
     <div className="relative flex content-start   gap-3 items-center w-full">
       <input
+        ref={inputRef}
         onChange={handleChange}
         placeholder={placeholder}
         className={`rounded-md border border-gray-300 px-3 py-2 pr-10 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500${className}`}
